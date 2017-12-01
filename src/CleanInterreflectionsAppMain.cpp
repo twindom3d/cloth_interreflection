cbceb00f0124e399ea8c9afb7ffd1df41dbebac3
David <david@twindom.com>
2017-12-01 12:28:48 -0800

    Select y-value cutoff

--- src/CleanInterreflectionsAppMain.cpp
+++ src/CleanInterreflectionsAppMain.cpp
@@ -15,7 +15,7 @@
 float YSlope(Halfedge_handle halfedge)
 {
   auto next_halfedge = halfedge;
-  for (int i = 0; i < 5; i++)
+  for (int i = 0; i < 3; i++)
   {
     next_halfedge = next_halfedge->next();
   }
@@ -45,15 +45,7 @@ int main(int argc, char* argv[])
   borders.calcBorderCentroids();
   borders.sortBorders();
   reverse(borders.borders_.begin(), borders.borders_.end());
-
-  for (auto& border : borders.borders_)
-  {
-    std::cout << border.edges.size() << " ";
-  }
-
-  std::cout << endl;
-
-  // borders.smoothBorders(3); // Smooth out jumps
+  borders.smoothBorders(10); // Smooth out jumps
 
   // Find arm borders
   // Assumed border furthest left = right arm and border furthest right = left arm
@@ -65,8 +57,7 @@ int main(int argc, char* argv[])
   for (int i = 0; i < 6; i++)
   {
     auto border = borders.borders_[i];
-    std::cout << "border size: " << border.edges.size() << endl;
-    std::cout << "centroid: " << border.centroid << endl;
+
     if (border.centroid.x() > left_arm_border.centroid.x())
     {
       left_arm_border = border;
@@ -103,37 +94,65 @@ int main(int argc, char* argv[])
   }
 
   // From starting point, travel towards body, comparing y-slopes along the way
-  // When slopes changes past threshold, choose this as minimum y
-
   // Left arm border
   // Only travel x% of the way around
+
+  vector<float> left_arm_slopes_differences;
   auto current_halfedge = start_left_border->next();
   for (int i = 0; i < left_arm_border.edges.size() * 0.3; i++)
   {
     auto current_slope = YSlope(current_halfedge);
     current_halfedge = current_halfedge->next();
-    auto next_slope = YSlope(current_halfedge);
+    auto three_forward = current_halfedge->next()->next();
+    auto next_slope = YSlope(three_forward);
     float slope_diff = next_slope - current_slope;
+    left_arm_slopes_differences.push_back(slope_diff);
     // std::cout << "slope_diff: " << slope_diff << " at pt: " << current_halfedge->vertex()->point() << endl;
   }
 
-  std::cout << endl << endl << endl;
-
   // Right arm border
+  vector<float> right_arm_slopes_differences;
   current_halfedge = start_right_border->next();
   for (int i = 0; i < right_arm_border.edges.size() * 0.3; i++)
   {
     auto current_slope = YSlope(current_halfedge);
     current_halfedge = current_halfedge->next();
-    auto next_slope = YSlope(current_halfedge);
+
+    auto three_forward = current_halfedge->next()->next();
+    auto next_slope = YSlope(three_forward);
     float slope_diff = next_slope - current_slope;
+    right_arm_slopes_differences.push_back(slope_diff);
     // std::cout << "slope_diff: " << slope_diff << " at pt: " << current_halfedge->vertex()->point() << endl;
   }
 
-  std::cout << "start left: " << start_left_border->vertex()->point() << endl;
-  std::cout << "start right: " << start_right_border->vertex()->point() << endl;
+  // Chose y-value at maximum slope diff
+  auto max_slope_left = max_element(left_arm_slopes_differences.begin(), left_arm_slopes_differences.end());
+  auto vertex_from_start_left = distance(left_arm_slopes_differences.begin(), max_slope_left);
+  auto max_slope_right = max_element(right_arm_slopes_differences.begin(), right_arm_slopes_differences.end());
+  auto vertex_from_start_right = distance(right_arm_slopes_differences.begin(), max_slope_right);
+
+  Halfedge_handle cutoff_halfedge_left = start_left_border;
+  for (int i = 0; i < vertex_from_start_left; i++)
+  {
+    cutoff_halfedge_left = cutoff_halfedge_left->next();
+  }
+
+  Halfedge_handle cutoff_halfedge_right = start_right_border;
+  for (int i = 0; i < vertex_from_start_right; i++)
+  {
+    cutoff_halfedge_right = cutoff_halfedge_right->next();
+  }
+
+  auto cutoff_y_left = cutoff_halfedge_left->vertex()->point().y();
+  auto cutoff_y_right = cutoff_halfedge_right->vertex()->point().y();
+
+  std::cout << "cutoff left: " << cutoff_y_left << endl;
+  std::cout << "cutoff right: " << cutoff_y_right << endl;
+  // std::cout << "start left: " << start_left_border->vertex()->point() << endl;
+  // std::cout << "start right: " << start_right_border->vertex()->point() << endl;
 
   // Delete faces in arm hole below min Y value
+  
 
   // Save off file
 }
